661bad27686ba96f145a3f3b87c7981ddb52d5cd
Young <485416879@qq.com>
2023-01-27 11:45:15 +0800

    跳过Echarts，准备权限管理前，尝试代码生成器备份

--- vue/src/pages/Home.vue
+++ vue/src/pages/Home.vue
@@ -1,18 +1,47 @@
 <template>
-  <div>这是主页的内容</div>
+  <div>
+    <div id="main" style="height: 500px;width: 800px"></div>
+  </div>
 </template>
 
 <script>
+//引入Echarts
+import * as echarts from 'echarts'
+
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Home",
+  data(){
+    return{
+
+    }
+  },
   mounted() {
-    this.$store.state.paths = [{
-      name: '首页',
-      path: '/home'
-    }]
+    //绘制图表
+    let chartDom = document.getElementById('main')
+    let myChart = echarts.init(chartDom)
+    let option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line'
+        }
+      ]
+    }
+    myChart.setOption(option)
   }
+
 }
+
+
+
 </script>
 
 <style scoped>
